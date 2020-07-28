8d9e185d3ed365eb72237198818f60b3a6666980
JhojanGuille <guille.balvin@gmail.com>
2020-07-27 19:02:00 -0500

    Eureka Server

---
+++ src/main/java/com/jguiller/EurekaBankService/EurekaBankServiceApplication.java
@@ -0,0 +1,15 @@
+package com.jguiller.EurekaBankService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaBankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jguiller/EurekaBankService/EurekaBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jguiller.EurekaBankService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaBankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
